851afa9f6cc6cfac93eb5336575fa3120330d976
mahmud-21 <mmr106406@gmail.com>
2026-06-16 00:19:50 +0600

    Update Rectangle.java

--- shapes/Rectangle.java
+++ shapes/Rectangle.java
@@ -1,36 +1,24 @@
 package shapes;
 
 public class Rectangle extends Shape {
-
-    private double width;
-    private double length;
+    private double width, length;
 
     public Rectangle(String color, boolean filled, double width, double length) {
         super(color, filled);
-        this.width  = width;
+        this.width = width;
         this.length = length;
     }
 
-    @Override
-    public double getArea() {
-        return width * length;
-    }
-
-    @Override
-    public double getPerimeter() {
-        return 2 * (width + length);
-    }
-
-    @Override
-    public String toString() {
-        return "[ Rectangle ]\n" +
-                super.toString()  + "\n" +
-                "Width : " + width  + "\n" +
-                "Length: " + length + "\n" +
-                String.format("Area      : %.2f", getArea())      + "\n" +
-                String.format("Perimeter : %.2f", getPerimeter());
-    }
-
-    public double getWidth()  { return width;  }
+    public double getArea() { return width * length; }
+    public double getPerimeter() { return 2 * (width + length); }
+    public double getWidth() { return width; }
     public double getLength() { return length; }
+
+    public String toString() {
+        return "[ Rectangle ]\n" + super.toString() + "\n" +
+               "Width : " + width + "\n" +
+               "Length: " + length + "\n" +
+               "Area      : " + String.format("%.2f", getArea()) + "\n" +
+               "Perimeter : " + String.format("%.2f", getPerimeter());
+    }
 }
